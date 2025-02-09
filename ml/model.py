9f3d8ed1218e7e880d75eb5f33a8038c81196474
jcsmithpcb <121827588+jcsmithpcb@users.noreply.github.com>
2025-02-09 16:38:09 -0700

    Update model.py
    update to Model.py for Step 3

--- ml/model.py
+++ ml/model.py
@@ -1,7 +1,9 @@
 import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
-# TODO: add necessary import
+import pandas as pd
+import numpy as np
+
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -19,11 +21,17 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
-    # TODO: implement the function
-    pass
+    model = RandomForestClassifier(
+        n_estimators=100,  # Number of trees in the forest
+        random_state=42,   # Seed for reproducibility
+        max_depth=None,    # No maximum depth for the trees
+        min_samples_split=2,  # Minimum samples required to split an internal node
+        min_samples_leaf=1    # Minimum samples required for a leaf node
+    )
+	model.fit(X_train, y_train)
+	return model
 
-
-def compute_model_metrics(y, preds):
+def comwpute_model_metrics(y, preds):
     """
     Validates the trained machine learning model using precision, recall, and F1.
 
@@ -59,8 +67,8 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    # TODO: implement the function
-    pass
+	preds = model.predict(X)  
+    return preds
 
 def save_model(model, path):
     """ Serializes model to a file.
@@ -72,13 +80,14 @@ def save_model(model, path):
     path : str
         Path to save pickle file.
     """
-    # TODO: implement the function
-    pass
+    with open(path, 'wb') as file:
+        pickle.dump(model, file)
 
 def load_model(path):
     """ Loads pickle file from `path` and returns it."""
-    # TODO: implement the function
-    pass
+    with open(path, 'rb') as file:
+        model = pickle.load(file)  # Deserialize the model/object from the specified path
+    return model
 
 
 def performance_on_categorical_slice(
@@ -117,12 +126,15 @@ def performance_on_categorical_slice(
     fbeta : float
 
     """
-    # TODO: implement the function
+    data_slice = data[data[column_name] == slice_value]
     X_slice, y_slice, _, _ = process_data(
-        # your code here
-        # for input data, use data in column given as "column_name", with the slice_value 
-        # use training = False
+        data_slice,
+        categorical_features=categorical_features,
+        label=label,
+        training=False,
+        encoder=encoder,
+        lb=lb,
     )
-    preds = None # your code here to get prediction on X_slice using the inference function
+    preds = inference(model, X_slice)
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
