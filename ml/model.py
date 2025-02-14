e99ae9913728552762c7afa0c445c9d2869a06c2
jcsmithpcb <121827588+jcsmithpcb@users.noreply.github.com>
2025-02-14 14:14:22 -0700

    Update model.py - fixing tab/space issues

--- ml/model.py
+++ ml/model.py
@@ -119,7 +119,7 @@ def performance_on_categorical_slice(
 		recall : float
 		fbeta : float
 		"""
-	data_slice = data[data[column_name] == slice_value]
+	data_slice = data[data[column_name] == slice_value] 
 	X_slice, y_slice, _, _ = process_data(
 		data_slice,
 		categorical_features=categorical_features,
