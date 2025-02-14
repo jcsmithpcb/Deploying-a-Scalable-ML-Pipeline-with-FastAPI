e67eab48c78a3c966ca165c4d9d8bd2616b41859
jcsmithpcb <121827588+jcsmithpcb@users.noreply.github.com>
2025-02-14 15:10:21 -0700

    Update test_ml.py with Unit Tests

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,51 @@
 import pytest
 # TODO: add necessary import
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from ml.model import train_model, inference, compute_model_metrics
+from ml.data import process_data
+import pandas as pd
 
 # TODO: implement the first test. Change the function name and input as needed
 def test_one():
     """
-    # add description for the first test
+    Test if train_model function returns a trained RandomForestClassifier model
     """
     # Your code here
+    X_train = np.array([[0, 1, 2], [1, 0, 3], [2, 1, 0]])
+    y_train = np.array([0, 1, 0])
+
+    model = train_model(X_train, Y_train)
+    assert isinstance(model, RandomForestClassifier), "train_model did not return a RandomForestClassifier instance"
     pass
 
 
 # TODO: implement the second test. Change the function name and input as needed
 def test_two():
     """
-    # add description for the second test
+    Test is inference function returns the correct number of predictions
     """
     # Your code here
+    X_test = np.array([[0, 1, 2], [1, 0, 3], [2, 1, 0]])
+    y_test = np.array([0, 1, 0])
+
+    model = train_model(X_test, y_test)
+    preds = inference(model, X_test)
+    assert len(preds) == len(X_test), "Inference function did not return the correct number of predictions"
     pass
 
 
 # TODO: implement the third test. Change the function name and input as needed
 def test_three():
     """
-    # add description for the third test
+    Test if compute_model_metrics function returns precision, recall, and fbeta score within valid ranges
     """
     # Your code here
+    y_true = np.array([0, 1, 0, 1])
+    y_preds = np.array([0, 1, 1, 1])
+
+    precision, recall, fbeta = compute_model_metrics(y_true, y_preds)
+    assert 0 <= precision <= 1, "Precision is out of range"
+    assert 0 <= recall <= 1, "Recall is out of range"
+    assert 0 <= fbeta <= 1, "Fbeta score is out of range"    
     pass
